44d973df8270212183fc937e8e20777b45dd8ed4
tymiao1220 <tianyimiao@Tianyis-MBP.fios-router.home>
2019-06-03 21:32:28 -0400

    013 014

--- 010 Regular Expression Matching.js
+++ 010 Regular Expression Matching.js
@@ -45,11 +45,21 @@ p = "mis*is*p*."
 Output: false
 */
 
+
+/*
+Backtraking
+*/
+
+
 /**
  * @param {string} s
  * @param {string} p
  * @return {boolean}
  */
+
+
+
 var isMatch = function(s, p) {
-    
+    let reg=new RegExp("^"+p+"$");
+    return reg.test(s);
 };
