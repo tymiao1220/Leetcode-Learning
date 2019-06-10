1ba45481bb79ad7fc0e90ea20270443c4bd6790b
Miao <miaot2@fr-n-s087087.ncifcrf.gov>
2019-06-10 17:44:19 -0400

    018

--- 018 4Sum.js
+++ 018 4Sum.js
@@ -34,18 +34,31 @@ var fourSum = function(nums, target) {
 
     if(nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target){ break; }
     if(nums[i] + nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3] < target){ continue; }
-    if(nums[i] === nums[i + 1]) continue;
+    /*
+      Should will compare with pre not next(will miss [-3(0),-3(1),*,*] solution)
+      duplicate quadruplets does not mean duplicate element in array:
+      [-3(0),*,*] === [-3(1),*,*] because they will all loop through rest
+      but if compare with pre will miss [-3(0),-3(1),*] sulotion
+    */
+    if(i > 0 && nums[i] === nums[i - 1]) continue;
+    // if(nums[i] === nums[i + 1]) continue;
 
     for(let j = nums.length - 1; j > 2; j--){
       if(nums[j] + nums[j - 1] + nums[j - 2] + nums[j - 3] < target){ break; }
       if(nums[j] + nums[i] + nums[i + 1] + nums[i + 3] > target){ continue; }
-      if(nums[j] === nums[j - 1]) continue;
+
+      if(j < nums.length - 1 && nums[j] === nums[j + 1]) continue;
+      // if(nums[j] === nums[j - 1]) continue;
       let secIndex = i + 1, thrIndex = j - 1;
       while(secIndex < thrIndex){
         if(nums[i] + nums[secIndex] + nums[thrIndex] + nums[j] === target){
           Output.push([nums[i], nums[secIndex], nums[thrIndex], nums[j]]);
           while(nums[secIndex] === nums[secIndex + 1]) { secIndex ++; }
           while(nums[thrIndex] === nums[thrIndex - 1]) { thrIndex --; }
+
+          /* Don't forget update iterator especially for while loop*/
+          secIndex ++;
+          thrIndex --;
         }
         else if(nums[i] + nums[secIndex] + nums[thrIndex] + nums[j] > target){
           thrIndex --;
